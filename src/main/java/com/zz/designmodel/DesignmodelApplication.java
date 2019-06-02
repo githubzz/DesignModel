56c88fd7c6eec5906049f9582f791f94b679db43
zengzhen <373627318@qq.com>
2019-06-02 13:47:54 +0800

    初始化提交

---
+++ src/main/java/com/zz/designmodel/DesignmodelApplication.java
@@ -0,0 +1,13 @@
+package com.zz.designmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignmodelApplication.class, args);
+	}
+
+}
